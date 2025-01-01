3206ddc1dabc586054cece172c227b73479876f1
Aleena <aleenaraichelabey@gmail.com>
2025-01-02 00:46:10 +0530

    add task

---
+++ add.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Task</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="addAction.php" method="post">
+        <h2>Add Task</h2>
+        <label>Task</label>
+        <input type="text" name="task" required><br>
+        <button type="submit">Add</button>
+        <a href="home.php" class="btn">Back to Home</a>
+    </form>
+</body>
+</html>
+
+
+
+
+
+
